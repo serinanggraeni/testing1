dbea0401ae30d86bd93f589ad31d44968e87c5c7
serinanggraeni <9b.27.serinanggraaeni@gmail.com>
2025-06-24 11:28:53 +0700

    Update index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+export function kucing(){
+    console.log("akukucing")
+}
+
+function tes(){
+    const tes1 = 1
+    console.log(tes1)
+
+    let tes2 = 2
+    console.log(tes2)
+    
+    tes2 = 3
+    console.log(tes2)
+}
+
+tes()
